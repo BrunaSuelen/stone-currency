61cf53564c70256c645102d4b4325b5375dbb1f8
Bruna Suelen <bruna.srgs@gmail.com>
2021-06-06 15:28:37 -0300

    feat: Cria página principal
    Cria componentes Home para renderizar o formulário.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
 
+import Form from "../../components/Form/Form";
 export default class Home extends Component {
-  render() {}
+  
+  teste(e) {
+    console.log(e)
+  }
+
+  render() {
+    return (
+      <Form changeTeste={(e) => this.teste(e)}></Form>
+    );
+  }
 }
