881bd8318ff90ba450dd669d84bb021ed9ec8c22
Bruna Suelen <bruna.srgs@gmail.com>
2021-06-06 21:48:51 -0300

    refactor: Adiciona hook ao form
    Declara atributo hook para receber modificações dos campos através do método onChangeInput, responsável por atualizar os dados do fomulário.

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
-
-import './Form.scss'
+import React, { useState } from 'react';
 import InputText from '../InputText/InputText';
 
-const Form = (props) => {
-  const { changeTeste } = props;
+import './Form.scss'
+
+const Form = () => {
+  const [formValues, setFormValues] = useState({});
+
+  function onChangeInput(element) {
+    let dataToSend = {...formValues};
+    dataToSend[element.id] = element.value;
+
+    setFormValues(dataToSend);
+  }
   
   return (
     <form>
-      <InputText label='Teste' id='teste' change={(e) => changeTeste(e)} />
-      <InputText label='Teste1' id='teste1' change={(e) => changeTeste(e)} />
+      <InputText 
+        id='dollar'
+        label='Dólar'
+        change={(e) => onChangeInput(e.target)}
+      />
+
+      <InputText
+        id='stateFee'
+        label='stateFee'
+        change={(e) => onChangeInput(e.target)}
+      />
     </form>
   );
 }
