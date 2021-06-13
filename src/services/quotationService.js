5f1417f78af48fdf46f43214c0704082a52144b7
Bruna Suelen <bruna.srgs@gmail.com>
2021-06-13 18:19:12 -0300

    feat: Adiciona metodo para conversão
    Implementa metodo que converte dolar para real diferenciando calculo pela taxa de dinheiro e cartão.

--- src/services/quotationService.js
+++ src/services/quotationService.js
@@ -1,13 +1,19 @@
 import axios from 'axios';
 
+const IOF = { cash: 1.1, card: 6.4 };
+
 function getQuotation() {
-  axios.get('https://economia.awesomeapi.com.br/json/last/USD-BRL')
-  .then((response) => {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
+  return axios.get('https://economia.awesomeapi.com.br/json/last/USD-BRL');
 }
 
-export default getQuotation;
+function dollarToRealConverter(data) {
+  const { isCash, dollar, stateFee, quotation } = data;
+  const IOFCalculated = (dollar * IOF[isCash ? 'cash' : 'card']) / 100;
+  const FeeCalculated = (dollar * stateFee) / 100;
+  
+  return isCash
+    ? (dollar + FeeCalculated) * (quotation + IOFCalculated)
+    : (dollar + FeeCalculated + IOFCalculated) * quotation;
+}
+
+export { getQuotation, dollarToRealConverter };
