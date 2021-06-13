70108896d2f0ba6a933cf22f8b3256543f5c2497
Bruna Suelen <bruna.srgs@gmail.com>
2021-06-13 18:22:23 -0300

    feat: Renderização do valor final
    Adicona metodo para envio dos dados do formuário para o metodo do service responsável por converter os valores.

--- src/components/Conversion/Conversion.jsx
+++ src/components/Conversion/Conversion.jsx
@@ -1,9 +1,35 @@
 import React, { useState } from 'react';
 
-const Conversion = () => {
- 
+import { getQuotation, dollarToRealConverter } from '../../services/quotationService'
+
+const Conversion = (props) => {
+  const { formValues } = props;
+  const [result, setResult] = useState(0);
+
+  getConversion();
+
+  function getConversion() {
+    getQuotation()
+      .then(response => {
+        let quotation = parseFloat(response.data.USDBRL.bid);
+      
+        if (quotation) {
+          const dataToConversion = {...formValues};
+          dataToConversion.quotation = quotation;
+          const convertedValue = dollarToRealConverter(dataToConversion);
+          setResult(convertedValue.toFixed(2));
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
   return (
-    <p>Conversion</p>
+    <div>
+      <h5>O resultado do cálculo é</h5>
+      <h1>{result}</h1>
+    </div>
   );
 }
   
